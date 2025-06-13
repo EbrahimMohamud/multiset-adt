1c2bffccb857512470068c3e17e78e93b7d0a1c7
Ebrahim Mohamud <ebrahim.mohamud@mail.utoronto.ca>
2025-06-13 18:36:11 -0400

    Almost Done

--- src/TreeMultiSet.java
+++ src/TreeMultiSet.java
@@ -1,15 +1,33 @@
 public class TreeMultiSet extends MultiSet {
-
-    // TODO add a Tree object as a private instance variable for this class.
-    /**
-     * Add the given item to this multiset.
-     *
+    private Tree tree;
+    /** Add the given item to this multiset.
      * @param item the item to add
      */
+    public TreeMultiSet() {
+        this.tree = new Tree(null);
+    }
     @Override
     void add(Integer item) {
-        // TODO complete this method
+        this.tree.insert(item);
     }
-    // TODO write the rest of the implementation for this class so that it uses its private Tree
-    //      object to provide the MultiSet functionality.
+    @Override
+    void remove(Integer item) {
+        this.tree.delete(item);
+    }
+    @Override
+    boolean contains(Integer item) {
+        return this.tree.contains(item);
+    }
+    @Override
+    boolean isEmpty() {
+        return this.tree.isEmpty();
+    }
+    @Override
+    int count(Integer item) {
+        return this.tree.count(item);
+    }
+    @Override
+    int size() {
+        return this.tree.len();
+    }
 }
