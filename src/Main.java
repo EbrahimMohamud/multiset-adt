a772c2145df1e8dc0a0d1ac7649a95195c5e4bd0
Ebrahim Mohamud <ebrahim.mohamud@mail.utoronto.ca>
2025-06-15 15:15:46 -0400

    Completed!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,44 @@
+import java.lang.classfile.constantpool.IntegerEntry;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
+
+    public static void profileMultiSet(MultiSet myInput, int n) {
+        ArrayList<Integer> itemAdded = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int x = ThreadLocalRandom.current().nextInt(0, 100);
+            myInput.add(x);
+            itemAdded.add(x);
+        }
+        assert itemAdded.size() == n;
+
+        long start = System.currentTimeMillis();
+
+        for (Integer item : itemAdded) {
+            myInput.remove(item);
+        }
+        long end = System.currentTimeMillis();
+
+        assert myInput.isEmpty();
+    }
+
     public static void main(String[] args) {
-        // TODO complete this psvm main method to replicate the timing experiment from adts.py.
-        //      Note: this will be client code of the various other classes needing to be written.
+        ArrayList<MultiSet> multiSet = new ArrayList<>(
+                Arrays.asList(
+                        new TreeMultiSet(),
+                        new ArrayListMultiSet(),
+                        new LinkedListMultiSet()
+                )
+        );
+        int[] values = {500, 1000, 2000, 4000};
+
+        for (MultiSet set : multiSet) {
+            for (Integer n : values) {
+                profileMultiSet(set, n);
+            }
+        }
     }
 }
